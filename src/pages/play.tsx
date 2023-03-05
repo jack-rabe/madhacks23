0f5575f3df547080be0e778f4f421596439e025f
Ryan Almizyed <almizyed@wisc.edu>
2023-03-05 03:31:49 -0600

    Remove banner from play.tsx

--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -3,7 +3,6 @@ import MyMap from "../components/MyMap";
 export default function Home() {
   return (
     <>
-      <div className="font-bold text-4xl navbar bg-primary mb-8">WisGo</div>
       <MyMap></MyMap>
     </>
   );
