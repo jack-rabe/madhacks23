47b4f448dbc2cde5f4deb40000f9cd430fdd96ad
Jack Rabe <jhrabe@wisc.edu>
2023-03-05 01:24:29 -0600

    some leaderboard fixes

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -43,16 +43,14 @@ export default function Leaderboard() {
       <h1 className="text-center text-4xl m-3 font-bold text-red-800">
         Leaderboard
       </h1>
-      <div className="mx-auto rounded-md flex hover:bg-red-900 justify-center bg-red-800 my-1 px-4 text-black w-3/4 ">
+      <div className="w-3/4 mx-auto rounded-md flex hover:bg-red-900 justify-center bg-red-800 my-1 px-4 text-black">
         <>Position</>
         <div className="divider divider-horizontal h-100"></div>
         <>Name</>
         <div className="divider divider-horizontal h-100"></div>
         <>Score</>
       </div>
-      <div className="flex flex-col items-center overflow-scroll">
-        {userCards}
-      </div>
+      <div className="flex flex-col items-center">{userCards}</div>
     </>
   );
 }
@@ -70,7 +68,7 @@ function PlayerCard({
 }) {
   return (
     <div
-      className={`rounded-md flex hover:bg-red-900 justify-center bg-red-800 m-1 px-4 text-black ${
+      className={`rounded-md flex hover:bg-red-900 justify-center bg-red-800 m-1 px-4 text-black w-3/4 ${
         isCurrent ? "text-white" : ""
       }`}
     >
