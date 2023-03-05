604fc06a79cd5fb1e08beda05e8819bca043d2c4
bluepra <parora9@wisc.edu>
2023-03-05 11:07:12 -0600

    fix 500s

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import { useRouter } from "next/router";
 import { getUser } from "./leaderboard";
 import React from "react";
-import ReactPlayer from 'react-player';
-
-
+import ReactPlayer from "react-player";
 
 export default function Home() {
   const router = useRouter();
@@ -25,14 +23,14 @@ export default function Home() {
           PLAY
         </p>
       </div>
-      <input 
-          placeholder="Input username here..."
-          className="input"
-          onBlur={(e) => {
-            if (!localStorage.getItem("username")) createUser(e.target.value);
-          }}
-        ></input >
-      <video src="/videos/videoslow.mp4" preload='auto' autoPlay loop muted />
+      <input
+        placeholder="Input username here..."
+        className="input"
+        onBlur={(e) => {
+          if (!localStorage.getItem("username")) createUser(e.target.value);
+        }}
+      ></input>
+      <video src="/videos/videoslow.mp4" preload="auto" autoPlay loop muted />
     </div>
   );
 }
@@ -40,14 +38,14 @@ export default function Home() {
 async function createUser(username: string) {
   if (username === "") return;
   const password = crypto.randomUUID();
-  localStorage.setItem("username", username);
-  localStorage.setItem("password", password);
 
   const data = await fetch("/api/user/create", {
     method: "POST",
-    body: JSON.stringify(getUser(localStorage)),
+    body: JSON.stringify({ username: username, password: password }),
   });
   const res = await data.json();
+  localStorage.setItem("username", username);
+  localStorage.setItem("password", password);
   console.log(res);
   console.log(`created user ${username}`);
 }
