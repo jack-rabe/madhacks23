44c9ce83826d2881668c3688d69aa310545a4409
Sofya Akhetova <akhetova@wisc.edu>
2023-03-04 20:57:19 -0600

    front end shananigan

--- src/pages/practice.tsx
+++ src/pages/practice.tsx
@@ -3,8 +3,14 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div className="mainbox">
-      <p>Hello, world!</p>
+    <div>
+      <div className="mainbox">
+        <p className="titlegame">WISGO</p>
+        <p className="titlegamemini">Explore UW Madison like never before!</p>
+      </div>
+      <div className="buttonplay">
+      <p className="buttontext">PLAY</p>
+      </div>
     </div>
   );
 }
